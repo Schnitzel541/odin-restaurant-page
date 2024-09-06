61ba637278d005be2e1146a2c5ffb54ae921966e
Schnitzel541 <j.budding122@gmail.com>
2024-09-06 16:42:02 +0200

    Remove innerHTML attribute from submitButton

--- src/contact.js
+++ src/contact.js
@@ -25,15 +25,18 @@ export const contact = () => {
     form.appendChild(createLabel("email", "E-mail:"));
     form.appendChild(createInput("email", "email"));
     form.appendChild(createLabel("text-area", "Message:"));
+
+    // Text area separate from functions due to different HTML attributes
     const textArea = document.createElement("textarea");
     textArea.rows = "10";
     textArea.cols = "75";
     form.appendChild(textArea);
 
+    // Submit button separate from functions due to different HTML attributes
     const submitButton = document.createElement("input");
     submitButton.type = "submit";
     submitButton.id = "submit-button";
-    submitButton.textContent = "Submit";
+    submitButton.innerHTML = "<span>Submit Form</span>"
     form.appendChild(submitButton);
 
     contentDiv.appendChild(form);

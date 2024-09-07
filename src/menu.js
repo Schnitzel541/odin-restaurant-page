e282a3a48422c74d695c27aebfc6d1127c2f4695
Schnitzel541 <j.budding122@gmail.com>
2024-09-07 12:25:07 +0200

    Add element creation function

--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,27 @@
 const contentDiv = document.querySelector("#content");
 
+const domLogic = (function () {
+    const addImage = (fileName) => {
+        const image = document.createElement("img");
+        image.src = `../assets/${fileName}`;
+        contentDiv.appendChild(image);
+    }
 
-const addImage = (fileName) => {
-    const image = document.createElement("img");
-    image.src = `../assets/${fileName}`;
-    contentDiv.appendChild(image);
-}
+    const createElement = (element, textContent) => {
+        const newElement = document.createElement(`${element}`);
+        newElement.textContent = textContent;
+        contentDiv.appendChild(newElement);
+    }
+
+    return { addImage, createElement };
+})();
+
+
 
 export const menu = () => {
-    addImage("restaurant.jpg");
+    domLogic.createElement("h3", "Schnitzel with lemon");
+    domLogic.addImage("schnitzel.jpg");
+
+    domLogic.createElement("h3", "Turkish bread");
+    domLogic.addImage("turkish-bread.jpg")
 };

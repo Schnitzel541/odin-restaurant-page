e9ee1d55296244ea1880897aec9e6ab086d366ea
Schnitzel541 <j.budding122@gmail.com>
2024-09-08 13:14:35 +0200

    Add lemon cake

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,6 @@
 import schnitzel from "../assets/schnitzel.jpg";
 import turkishBread from "../assets/turkish-bread.jpg";
-
+import lemonCake from "../assets/lemon-cake.jpg";
 
 const contentDiv = document.querySelector("#content");
 
@@ -24,10 +24,14 @@ const domLogic = (function () {
 
 export const menu = () => {
     domLogic.createElement("h3", "Schnitzel with lemon");
-    domLogic.createElement("p", "A delicious, spicy schnitzel with extra lemon flavouring")
+    domLogic.createElement("p", "A delicious, spicy schnitzel with extra lemon flavouring");
     domLogic.addImage(schnitzel);
 
     domLogic.createElement("h3", "Turkish bread");
-    domLogic.createElement("p", "Break away and eat!")
+    domLogic.createElement("p", "Break away and eat!");
     domLogic.addImage(turkishBread);
+
+    domLogic.createElement("h3", "Strawberry lemon cake");
+    domLogic.createElement("p", "Powerful, fruity and robust!");
+    domLogic.addImage(lemonCake);
 };
